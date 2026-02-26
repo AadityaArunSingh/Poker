443e2d93d21f5628bae161706dd3a50cad22e4f8
AadityaArunSingh <aadityas2111@gmail.com>
2026-02-26 16:32:31 +0000

    df

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as str
+import pandas as pd
 
 data = 'https://docs.google.com/spreadsheets/d/1N0f0momimoEEWxqmxSrthV3IxkQIMpxczoLIbHw5XsQ/edit?usp=sharing'
 
-str.table(data=data, border=True)
+df = pd.read_csv(data)
+
+str.table(data=df, border=True)
