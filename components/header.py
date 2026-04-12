6be9d4c5fd1a53e7c21af2d6e4924b21a1958442
AadityaArunSingh <aadityas2111@gmail.com>
2026-04-12 17:55:36 +0100

    name change

--- components/header.py
+++ components/header.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 def render_header():
     st.markdown('<div class="suit-row">♠ ♥ ♦ ♣</div>', unsafe_allow_html=True)
-    st.markdown('<div class="hero-title">JUAARI DASHBOARD</div>', unsafe_allow_html=True)
+    st.markdown('<div class="hero-title">POKER DASHBOARD</div>', unsafe_allow_html=True)
     st.markdown('<div class="hero-subtitle">Gambling addiction tracker</div>', unsafe_allow_html=True)
     st.markdown('<hr class="red-divider">', unsafe_allow_html=True)
